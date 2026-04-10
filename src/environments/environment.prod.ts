4bdfbbfc1c3ff1cb08c28880e6734e4c9f4dfcdd
Berno01 <ruedabernardo2@gmail.com>
2026-04-10 00:09:16 -0400

    Ajustes deploy prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://shellfruty.sistemastarija.com/public/api/v1/", // Configurar subdominio real aquí posteriormente
+  apiUrl: "https://flordeceviche.sistemastarija.com/public/api/v1/", // Configurar subdominio real aquí posteriormente
   cloudinary: {
     cloudName: "da8mb8dal", // TODO: Ajustar cuando se configure la cuenta de producción
     uploadPreset: "dunno_environment_dev", // TODO: Reemplazar por preset de producción
